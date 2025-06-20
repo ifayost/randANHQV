b8d5f6d2b4d141337288202232f09c3159fe3b25
ifayost <ifayost@gmail.com>
2025-06-20 19:41:51 +0200

    fix: wikipedia episode list scraping

--- episodedb.py
+++ episodedb.py
@@ -11,27 +11,32 @@ def get_episode_database(link, filename='./episodes.csv'):
     soup = BeautifulSoup(r.text, 'html.parser')
 
     tables = soup.find_all('table')
-    tables = [pd.read_html(StringIO(str(tables[i])))[0] for i in range(3, 3+2*5, 2)]
-    for i, table in enumerate(tables):
-        table = table.drop(['Audiencia', 'Fecha de emisión'], axis=1)
-        table = table.rename(
-                {'N.º (serie)': 'Temporada', 'N.º (temp.)': 'Episodio'},
-                axis=1
-                )
+    tables = [str(tables[i]) for i in range(3, 13, 1)]
+    tables = [t for t in tables if ('Audiencia' in t) | ('Fecha de emisión' in t)]
+    tables = [pd.read_html(StringIO(t))[0] for t in tables]
+    i = 0
+    for table in tables:
+        if len(set(['Audiencia', 'Fecha de emisión']) - set(table.columns)) == 0:
+            table = table.drop(['Audiencia', 'Fecha de emisión'], axis=1)
+            table = table.rename(
+                    {'N.º (serie)': 'Temporada', 'N.º (temp.)': 'Episodio'},
+                    axis=1
+                    )
 
-        if i < 2:
-            table['Temporada'] = i+1
-        elif i == 2:
-            table.loc[table.Episodio < 14,'Temporada'] = i+1
-            table.loc[table.Episodio >= 14,'Temporada'] = i+2
-            table.loc[table.Episodio >= 14,'Episodio'] = range(1, 22)
-        elif i > 2:
-            table['Temporada'] = i+2
+            if i < 2:
+                table['Temporada'] = i+1
+            elif i == 2:
+                table.loc[table.Episodio < 14,'Temporada'] = i+1
+                table.loc[table.Episodio >= 14,'Temporada'] = i+2
+                table.loc[table.Episodio >= 14,'Episodio'] = range(1, 22)
+            elif i > 2:
+                table['Temporada'] = i+2
 
-        if i == 0:
-            final_table = table.copy()
-        else:
-            final_table = pd.concat([final_table, table], axis=0)
+            if i == 0:
+                final_table = table.copy()
+            else:
+                final_table = pd.concat([final_table, table], axis=0)
+            i += 1
 
     final_table.to_csv(filename, index=False)
 
