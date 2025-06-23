8c9581dc7aa0810798f9c05cbfd4b6237a03d910
ifayost <ifayost@gmail.com>
2025-06-23 19:40:09 +0200

    fix: retry password

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 import base64
 import os
 
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from getpass import getpass
@@ -33,18 +33,26 @@ def read_credentials(password=None, filename='./credentials'):
     with open(filename, 'rb') as f:
         salt, token = f.read().split(b'\n')
     
-    if password is None:
-        password = getpass('Enter password: ').encode()
+    done = False
+    while not done:
+        try:
+            if password is None:
+                password = getpass('Enter password: ').encode()
 
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=1_000_000,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(password))
-    f = Fernet(key)
-    username, password = f.decrypt(token).decode().split('\n')
+            kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=salt,
+                iterations=1_000_000,
+            )
+            key = base64.urlsafe_b64encode(kdf.derive(password))
+            f = Fernet(key)
+            username, password = f.decrypt(token).decode().split('\n')
+            done = True
+        except InvalidToken:
+            print('[!] Wrong password, try again.')
+            password = None
+            
     return username, password
 
 
